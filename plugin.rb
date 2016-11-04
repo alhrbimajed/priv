7b72b8ee6173929a508072755c009ff0906e0844
Mohamad Abras <mohamad.abras@gmail.com>
2016-11-05 00:07:50 +0200

    add plugin.rb file

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: ArabicTranslation
+# about: Custome translation for Arabic Language
+# version: 0.0.1
+# authors: mabras
+# url: https://bitbucket.org/mabras/discourse-locale-override/tree/master
