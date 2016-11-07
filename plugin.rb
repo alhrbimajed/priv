60e7c6ca08b59dedd8c7fbf051843cbdd242745d
Mohamad Abras <mohamad.abras@gmail.com>
2016-11-07 16:31:50 +0200

    bump version

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: ArabicTranslation
 # about: Custome translation for Arabic Language
-# version: 0.0.1
+# version: 0.0.2
 # authors: mabras
 # url: https://bitbucket.org/mabras/discourse-locale-override
